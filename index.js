bd6cbfc70dcc0c8f657f96dbf7f0c6b0251f50c8
Alex00Rem <88440823+Alex00Rem@users.noreply.github.com>
2021-10-26 20:27:58 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const numberofFilm = prompt('Сколько фильмов вы уже посмотрели');
-const lastMovie = prompt('Один из последних просмотреных фильмов');
-const review = prompt('На сколько оцените его?');
+const numberofFilm = +prompt('Сколько фильмов вы уже посмотрели');
+if (numberofFilm > 0){
+    for(let i = 0; i <= numberofFilm; i++) {
+        const lastMovie = prompt('Один из последних просмотреных фильмов');
+        const review = prompt('На сколько оцените его?');
+        while (lastMovie.lengt)
+    }
+}
+
+
+
 
 const personalMovieDB = {
     count: numberofFilm,
