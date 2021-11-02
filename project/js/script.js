d83aa1869e096c5db7c77db5bbf23fecbe2ed52b
Alex00Rem <88440823+Alex00Rem@users.noreply.github.com>
2021-11-02 20:30:10 +0300

    practic

--- project/js/script.js
+++ project/js/script.js
@@ -24,3 +24,18 @@ const movieDB = {
     ]
 };
 
+const promo__adv = document.querySelector('.promo__adv');
+promo__adv.textContent = ''
+
+const promo__genre= document.querySelector('.promo__genre') 
+promo__genre.textContent = 'драма'
+
+const list= document.querySelector('.promo__interactive-list');
+list.textContent = '';
+const newList = movieDB.movies.sort((film1,film2) => film1.localeCompare(film2));
+newList.forEach((film, index) => {
+    list.innerHTML += `
+    <li class="promo__interactive-item">№ ${index + 1}: ${film}
+    <div class="delete"></div>
+    </li>`
+})
